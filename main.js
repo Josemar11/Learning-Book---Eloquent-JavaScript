fcf38c67d15eead583b5cfd13e01a56afb856b13
Josemar <jsm_nascimento@hotmail.com>
2022-06-17 17:44:49 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log(luigisDebt);
+// → 105
+
+let one = 1, two = 2;
+console.log(one + two);
+// → 3
+
+var nombre = "Ayda";
+const greeting = "Hello ";
+console.log(greeting + nombre);
+// → Hello Ayda
+
+prompt("Enter passcode");
+
+console.log(Math.max(2, 4));
+// → 4
+console.log(Math.min(2, 4) + 100);
+// → 102
+
+let theNumber = Number(prompt("Pick a number"));
+console.log("Your number is the square root of " + theNumber * theNumber);
+
+let xNumber = Number(prompt("Pick a number"));
+if (!Number.isNaN(youNumber)) {
+  console.log("Your number is the square root of " + xNumber * xNumber);
+}
+
+
+
