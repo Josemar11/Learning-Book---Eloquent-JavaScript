e80eae19f0b4205df800ca5c0d739c830c0f9a53
Josemar <93160986+Josemar11@users.noreply.github.com>
2022-06-20 23:20:56 -0300

    For Loops

--- Program Structure/main.js
+++ Program Structure/main.js
@@ -53,3 +53,17 @@ if (false != true) {
     console.log("No surprise there.");
   }
 }
+
+for (let number = 0; number <= 12; number = number + 2) {
+  console.log(number);
+}
+// → 0
+// → 2
+//   … etcetera
+
+let resultX = 1;
+for (let counter = 0; counter < 10; counter = counter + 1) {
+  result = result * 2;
+}
+console.log(result);
+// → 1024
