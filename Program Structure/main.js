7628c5e728509eb5666a55977e11c24124a2868d
Josemar <93160986+Josemar11@users.noreply.github.com>
2022-06-21 18:01:05 -0300

    Breaking Out of a Loop

--- Program Structure/main.js
+++ Program Structure/main.js
@@ -67,3 +67,11 @@ for (let counter = 0; counter < 10; counter = counter + 1) {
 }
 console.log(result);
 // → 1024
+
+for (let current = 20; ; current = current + 1) {
+  if (current % 7 == 0) {
+    console.log(current);
+    break;
+  }
+}
+// → 21
