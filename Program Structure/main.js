5a9d1bc8872e08dcab47b98351a0602d384f396c
Josemar <93160986+Josemar11@users.noreply.github.com>
2022-06-23 16:47:58 -0300

    thursday update

--- Program Structure/main.js
+++ Program Structure/main.js
@@ -75,3 +75,22 @@ for (let current = 20; ; current = current + 1) {
   }
 }
 // → 21
+
+if (x == "value1") action1();
+else if (x == "value2") action2();
+else if (x == "value3") action3();
+else defaultAction();
+
+switch (prompt("What is the weather like?")) {
+  case "rainy":
+    console.log("Remember to bring an umbrella.");
+    break;
+  case "sunny":
+    console.log("Dress lightly.");
+  case "cloudy":
+    console.log("Go outside.");
+    break;
+  default:
+    console.log("Unknown weather type!");
+    break;
+}
